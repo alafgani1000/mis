1465a7066622aa52886fe752ebcb9e9f5881dc36
alafgani1000 <irhabyaganal625@gmail.com>
2019-06-14 05:35:58 +0700

    seeder role and permission

--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
@@ -11,6 +13,16 @@ class RolesAndPermissionsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        app()[Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $roleEmployee = Role::create(['name' => 'empoloyee']);
+        $roleSpt = Role::create(['name' => 'spt mis']);
+        $roleMgr = Role::create(['name' => 'mgr mis']);
+        $roleBoss = Role::create(['name' => 'boss']);
+
+         /*master*/
+         Permission::create(['name' => 'approval']);
+         Permission::create(['name' => 'create']);
+         Permission::create(['name' => 'reject']);
     }
 }
